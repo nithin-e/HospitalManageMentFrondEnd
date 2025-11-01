d869cb27d715c67f9ec8f91671791f0d667d5d66
NITHIN_360 <nithinbalakrishnan569@gmail.com>
2025-11-01 15:15:38 +0530

    checking protected route

--- src/App.tsx
+++ src/App.tsx
@@ -75,7 +75,8 @@ const App = () => {
                   <Route path="/payment-success" element={<DoctorPaymentSucces />} />
                                   
                   
-                  <Route path="/userprofile"  element={<ProtectedRoute><UserProfile /></ProtectedRoute>} />
+                  {/* <Route path="/userprofile"  element={<ProtectedRoute><UserProfile /></ProtectedRoute>} /> */}
+                  <Route path="/userprofile"  element={<UserProfile />} />
                   <Route path="/success" element={<UserProfile />} />
                    <Route path="/userWallet"  element={<ProtectedRoute><UserWallet /></ProtectedRoute>} />
                   <Route path="/video-call/:roomId" element={<ProtectedRoute><VideoCallPage /></ProtectedRoute>} />
