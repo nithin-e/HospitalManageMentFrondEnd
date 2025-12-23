781a52e3e1dc858c2c2de3bb30fd44f2233e0dca
NITHIN_360 <nithinbalakrishnan569@gmail.com>
2025-12-23 12:02:23 +0530

    adding change on succes page..

--- src/components/user/DoctorApplySuccessPage.tsx
+++ src/components/user/DoctorApplySuccessPage.tsx
@@ -297,6 +297,7 @@ const DoctorApplySuccessPage = () => {
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01"></path>
                   </svg>
                   Track Application
+                  
                 </button>
               </div>
               
@@ -334,5 +335,18 @@ const DoctorApplySuccessPage = () => {
     </div>
   );
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 export default DoctorApplySuccessPage;
